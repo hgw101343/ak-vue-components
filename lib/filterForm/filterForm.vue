26cf39a87ded8c056289e6cce77c5cdbc3f1ce0b
huanggengwu <yunyaokj@gmail.com>
2023-06-26 15:01:51 +0800

    feat：列表操作图标组件和展开更多搜索组件上传

---
+++ lib/filterForm/filterForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="filter-containe">
+    <slot />
+    <el-link type="primary" @click="change">
+      {{ isMany ? '收起' : '展开' }}
+      <i :class="isMany ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"></i>
+    </el-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FilterForm',
+    props: {
+      // 是否展示更多
+      isMany: {
+        type: Boolean,
+        default: false,
+      },
+      // 简易搜索下还存在的字段名
+      liveValues: {
+        type: Array,
+        default: () => [],
+      },
+    },
+    data() {
+      return {}
+    },
+    mounted() {
+      console.log('执行几次表单')
+    },
+    methods: {
+      change() {
+        this.$emit('change')
+      },
+    },
+  }
+</script>
+<style lang="scss" scoped></style>
---
+++ lib/operateIcon/operateIcon.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="table-operation_box">
+    <el-tooltip v-for="val in data1" :key="val.label" :content="val.label">
+      <i
+        :class="
+          (val.iconClass ? val.iconClass : getIcon(val)) + ' table-operation'
+        "
+        :style="'color:' + val.color"
+        @click.stop="clickIcon(val.type)"
+      />
+    </el-tooltip>
+    <el-dropdown v-if="data2.length" @command="clickItem">
+      <i class="el-icon-more table-operation" @click.stop />
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item
+          v-for="val in data2"
+          :key="val.label"
+          :command="val.type"
+        >
+          {{ val.label }}
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+
+<script>
+  // 常用的操作图标
+  const iconList = [
+    {
+      label: '编辑',
+      value: 'el-icon-edit',
+    },
+    {
+      label: '查看',
+      value: 'el-icon-view',
+    },
+    {
+      label: '删除',
+      value: 'el-icon-delete',
+    },
+  ]
+  export default {
+    name: 'OperateIcon',
+    props: {
+      dataList: {
+        type: Array[
+          {
+            lable: String, // 鼠标上移弹出文字
+            permission: String, // 权限
+            type: String, // 什么类型的点击事件通过click返回出去做业务判断
+            iconClass: String, //自定义图标class名字，没有就去当前页面常用的图标
+            color: String, // 图标颜色 默认跟着主题走
+          }
+        ],
+        default: () => [],
+      },
+      dataLength: {
+        type: Number,
+        default: 3,
+      },
+    },
+    data() {
+      const { dataList, dataLength } = this
+      const permissions = localStorage.getItem('PERMISSIONS')
+      const data = dataList.filter(
+        (i) => permissions.indexOf(i.permission) !== -1 || !i.permission
+      )
+      return {
+        data1:
+          data.length > dataLength
+            ? JSON.parse(JSON.stringify(data)).splice(0, dataLength - 1)
+            : data,
+        data2: data.length > dataLength ? data.splice(dataLength - 1) : [],
+      }
+    },
+    computed: {},
+    mounted() {},
+    methods: {
+      getIcon(val) {
+        const currentIcon = iconList.find((i) => i.label === val.label)
+        if (currentIcon) {
+          return currentIcon.value
+        }
+        return 'el-icon-more'
+      },
+      clickIcon(type) {
+        this.$emit('click', type)
+      },
+      clickItem(e) {
+        this.$emit('click', e)
+      },
+    },
+  }
+</script>
+<style lang="scss" scoped></style>
